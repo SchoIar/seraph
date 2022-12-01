e362d799ad96845e4ccfa7a585340f9f18ec0ac9
Anton <antonilic7@gmail.com>
2022-12-01 12:40:11 -0800

    Instructions for cloning repo and running selfbot

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Seraph: A discord selfbot 
 
+Discord selfbot created with python. 
+To execute the program, add a .env with your specified token, and run it via `python3 main.py`
--- cogs/clear.py
+++ cogs/clear.py
@@ -2,9 +2,10 @@ from discord.ext import commands
 class Clear(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
+    '''Clears all messages in selected channel'''
     @commands.command(name='clear', aliases=['clr'], pass_context=True)
-    async def clear(self, ctx, username=None):
+    async def clear(self, ctx):
         print("Called clear")
 
 def setup(bot):
