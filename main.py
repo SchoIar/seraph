6993f927dd3484f830056a970386022185960522
Anton <antonilic7@gmail.com>
2022-12-01 12:13:44 -0800

    Main account login done

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ load_dotenv()
 token = os.getenv('TOKEN')
 
 
-bot = commands.Bot(command_prefix='.', self_bot=True, case_sensitive=False)
+bot = commands.Bot(command_prefix='!', self_bot=True, case_sensitive=False)
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
+
+@bot.event
+async def on_connect():
+    print("Connected as %s" % (bot.user))
 
 bot.run(token, bot=False)
