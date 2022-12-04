de9efa37c43781c4195392b9f210d7f99ccd6dff
Anton <antonilic7@gmail.com>
2022-12-03 21:09:10 -0800

    improved clear to remove ratelimit

--- cogs/clear.py
+++ cogs/clear.py
@@ -9,29 +9,13 @@ class Clear(commands.Cog):
     async def clear(self, ctx):
         await ctx.message.delete()
         channel = ctx.channel
-        print(f"Clearing messages with {channel}")
+        print(f"Clearing messages in: {channel}")
         async for message in channel.history(limit=None).filter(lambda m: m.author == self.bot.user).map(lambda m: m):
             try:
                 time.sleep(1)#to avoid being ratelimited
                 await message.delete()
             except:
                 pass    
-
-    '''Mass creates GCs with selected user'''
-    @commands.command(name='seraph', aliases=['nuker','spammed'], pass_context=True)
-    async def seraph(self, ctx):
-        print('Called nuker')
-
-    '''Direct messages all friends selected message'''
-    @commands.command(name='dm', aliases=['massdm','told all my friends'])
-    async def dmall(self, ctx, *, message):#args=None):
-        await ctx.message.delete()
-        for friend in self.bot.user.friends:
-            try:
-                await friend.send(message)#args)
-            except:
-                print(f"could not message: {friend.name} ")
-                
 
 def setup(bot):
     bot.add_cog(Clear(bot))
---
+++ cogs/mass.py
@@ -0,0 +1,27 @@
+# Commands for Mass usage (ie. Mass DM)
+from discord.ext import commands
+import time;
+
+
+class Mass(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    '''Mass creates GCs with selected user'''
+    @commands.command(name='seraph', aliases=['nuker','spammed'], pass_context=True)
+    async def seraph(self, ctx):
+        print('Called nuker')
+
+    '''Direct messages all friends selected message'''
+    @commands.command(name='dm', aliases=['massdm','told all my friends'])
+    async def dmall(self, ctx, *, message):#args=None):
+        await ctx.message.delete()
+        for friend in self.bot.user.friends:
+            try:
+                await friend.send(message)#args)
+            except:
+                print(f"could not message: {friend.name} ")
+
+def setup(bot):
+    bot.add_cog(Mass(bot))
