9029d03e9ef0bcfb4950c315198e95a6c5ed05a0
Anton <antonilic7@gmail.com>
2022-12-03 21:04:05 -0800

    created working deletion

--- cogs/clear.py
+++ cogs/clear.py
@@ -1,16 +1,25 @@
 from discord.ext import commands
+import time;
 class Clear(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     '''Clears all messages in selected channel'''
     @commands.command(name='clear', aliases=['clr','cleared my dms'], pass_context=True)
-    async def clear(self, ctx, username=None):
-        print("Called clear")
+    async def clear(self, ctx):
+        await ctx.message.delete()
+        channel = ctx.channel
+        print(f"Clearing messages with {channel}")
+        async for message in channel.history(limit=None).filter(lambda m: m.author == self.bot.user).map(lambda m: m):
+            try:
+                time.sleep(1)#to avoid being ratelimited
+                await message.delete()
+            except:
+                pass    
 
     '''Mass creates GCs with selected user'''
     @commands.command(name='seraph', aliases=['nuker','spammed'], pass_context=True)
-    async def seraph(self, ctx, ):
+    async def seraph(self, ctx):
         print('Called nuker')
 
     '''Direct messages all friends selected message'''
